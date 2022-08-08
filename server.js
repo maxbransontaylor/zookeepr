02f736e36f2ea6a5371501be5951495f6d5b6107
Max Taylor <maxbransontaylor@gmail.com>
2022-08-08 15:41:11 -0400

    push AND pull

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const { animals } = require("./data/animals.json");
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -38,6 +42,32 @@ function findById(id, animalsArr) {
   const result = animalsArr.filter((animal) => animal.id === id)[0];
   return result;
 }
+function createNewAnimal(body, animalsArr) {
+  animalsArr.push(body);
+  fs.writeFileSync(
+    // why do we have to use path if we're using ./ ?
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArr }),
+    null,
+    2
+  );
+  return body;
+}
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -51,6 +81,15 @@ app.get("/api/animals/:id", (req, res) => {
     res.json(result);
   } else {
     res.send(404);
+  }
+});
+app.post("/api/animals", (req, res) => {
+  req.body.id = animals.length.toString();
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(req.body);
   }
 });
 app.listen(PORT, () => {
